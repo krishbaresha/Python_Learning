373176cfa87a6d21f119f343b3b7b23713ece7b0
krishbaresha <krishbaresha@gmail.com>
2026-06-11 11:37:19 -0700

    Greeting Program Done

---
+++ Lab Session 1 Tasks/greetings.py
@@ -0,0 +1,9 @@
+print("====*******====")
+print("====Welcome====")
+print("====*******====")
+name = input("Welcome Dear User Kindly Tell us Your Good Name: ")
+age = int(input("Your Age Please: "))
+print("Thanks For the information" \
+"Here is your Information")
+
+print(f"Hello {name},you are {age} years old!")
