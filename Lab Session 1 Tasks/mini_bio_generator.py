67b922985915add0065194cc3afec9d76bd12995
krishbaresha <krishbaresha@gmail.com>
2026-06-13 14:09:00 +0500

    Mini Bio Generator Task Done

--- Lab Session 1 Tasks/mini_bio_generator.py
+++ Lab Session 1 Tasks/mini_bio_generator.py
@@ -2,4 +2,4 @@ name = input("Enter your name: ")
 city = input("Enter your city: ")
 animal = input("Enter an animal: ")
 
-print(f"Hello! My name is {name}. I'm From {city}. My Favourite animal is {animal}.")
+print(f"Hello! My name is {name}. I'm From {city}. My Favourite animal is {animal}")
